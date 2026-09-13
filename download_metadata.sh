6fd054021426ade11b3af42cbd76db8bfb826f87
M20291 <mtsvshoips96@imageeditgpt.com>
2026-09-13 22:45:46 +0300

    Update script to download video

--- download_metadata.sh
+++ download_metadata.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 echo "Video URL: $VIDEO_URL"
-python3 -m yt_dlp --dump-json "$VIDEO_URL" > video_metadata.json
-echo "Done"
+echo "Downloading video..."
+python3 -m yt_dlp "$VIDEO_URL" -o "video.%(ext)s"
+echo "Download complete"
